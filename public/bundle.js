25b3abd5106ea21563d506e575ad7f392b5f05e4
Gulshakh Kaur <gulshakh@Gulshakhs-MacBook-Pro.local>
2017-02-21 09:54:51 -0800

    Rename example page

--- public/bundle.js
+++ public/bundle.js
@@ -27229,9 +27229,18 @@
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples!'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples!'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome examples page !!'
+	    )
 	  );
 	};
 
